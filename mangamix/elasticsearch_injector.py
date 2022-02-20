cd3cc320af0435470955a71c4ad7e048b6b88a25
staillebois <sylvain.taillebois@gmail.com>
2022-02-20 14:49:07 +0100

    Index animes

--- mangamix/elasticsearch_injector.py
+++ mangamix/elasticsearch_injector.py
@@ -1,4 +1,8 @@
 import logging
+import hashlib
+
+import elastic_transport
+import elasticsearch
 from elasticsearch import AsyncElasticsearch
 from elasticsearch.helpers import async_bulk
 
@@ -9,16 +13,31 @@ class ElasticsearchInjector:
 
     def __init__(self):
         self.logger = logging.getLogger(f'{__name__}.{__class__.__name__}')
-        self.es = AsyncElasticsearch(hosts=ES_HOST, http_auth=(ES_USER, ES_PASSWORD))
+        self.es = AsyncElasticsearch(hosts=ES_HOST, http_auth=(ES_USER, ES_PASSWORD), retry_on_timeout=True)
 
     async def index(self, animes: list[str]):
-        self.logger.debug(f'Start indexing {animes}')
-        await async_bulk(self.es, self.__gendata(animes))
+        self.logger.info(f'Try to index {len(animes)} animes')
+        self.logger.debug(f'indexing {animes}')
+
+        try:
+            index_succeed, errors = await async_bulk(client=self.es, actions=self.__create_doc(animes),
+                                                     raise_on_error=False)
+            self.logger.info(f'Indexing succeed: {index_succeed}, indexing failed: {len(errors)}')
+            for error in errors:
+                self.logger.debug(error)
+        except (elastic_transport.ConnectionError, elasticsearch.AuthenticationException) as e:
+            self.logger.warning(e)
 
     @staticmethod
-    async def __gendata(animes: list[str]):
+    async def __create_doc(animes: list[str]):
         for anime in animes:
             yield {
                 "_index": ES_INDEX,
-                "test": anime
+                "_id": ElasticsearchInjector.hash_name(anime),
+                "_source": {'name': anime},
+                "_op_type": 'create',
             }
+
+    @staticmethod
+    def hash_name(name: str):
+        return hashlib.sha256(name.encode('utf-8')).hexdigest()
--- mangamix/settings.py
+++ mangamix/settings.py
@@ -2,6 +2,6 @@ import os
 
 ES_HOST = os.getenv('ES_HOST', 'http://localhost:9200')
 ES_USER = os.getenv('ES_USER', 'elastic')
-ES_PASSWORD = os.getenv('ES_PASSWORD', 'whlaib58SI-8RF9h1heP')
+ES_PASSWORD = os.getenv('ES_PASSWORD', 'dzNIt6zKlFN5ynoOlnsD')
 ES_INDEX = os.getenv('ES_INDEX', 'mangamix')
 ANIME_URL = os.getenv('ANIME_URL', 'https://www.nautiljon.com/classements/tendance/anime/')
