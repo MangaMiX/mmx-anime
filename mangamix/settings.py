06c831082479a8da3e8423de7ffc5cc815daaa5d
staillebois <sylvain.taillebois@gmail.com>
2023-01-25 14:01:41 +0100

    Remove default settings

--- mangamix/settings.py
+++ mangamix/settings.py
@@ -1,8 +1,8 @@
 import os
 
 ES_HOST = os.getenv('ES_HOST', 'http://localhost:9200')
-ES_USER = os.getenv('ES_USER', 'elastic')
-ES_PASSWORD = os.getenv('ES_PASSWORD', 'dzNIt6zKlFN5ynoOlnsD')
+ES_USER = os.getenv('ES_USER')
+ES_PASSWORD = os.getenv('ES_PASSWORD')
 ES_INDEX = os.getenv('ES_INDEX', 'mangamix')
 MMX_ANIME_URL = os.getenv('MMX_ANIME_URL', 'https://myanimelist.net/topanime.php')
 MMX_EXTRACT_LIMIT = int(os.getenv('MMX_EXTRACT_LIMIT')) if os.getenv('MMX_EXTRACT_LIMIT') else None
