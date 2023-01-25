6e2cff00e77b0e9321780aed162f6aefd6bb2a16
staillebois <sylvain.taillebois@gmail.com>
2023-01-25 14:23:02 +0100

    Update settings

--- mangamix/settings.py
+++ mangamix/settings.py
@@ -1,10 +1,10 @@
 import os
 
-ES_HOST = os.getenv('ES_HOST', 'http://localhost:9200')
+ES_HOST = os.getenv('ES_HOST')
 ES_USER = os.getenv('ES_USER')
 ES_PASSWORD = os.getenv('ES_PASSWORD')
-ES_INDEX = os.getenv('ES_INDEX', 'mangamix')
+ES_INDEX = os.getenv('ES_INDEX')
 MMX_ANIME_URL = os.getenv('MMX_ANIME_URL', 'https://myanimelist.net/topanime.php')
 MMX_EXTRACT_LIMIT = int(os.getenv('MMX_EXTRACT_LIMIT')) if os.getenv('MMX_EXTRACT_LIMIT') else None
-MMX_SEARCH_SIZE = int(os.getenv('MMX_EXTRACT_LIMIT', '50'))
+MMX_SEARCH_SIZE = int(os.getenv('MMX_SEARCH_SIZE', '50'))
 MMX_ANIMES = os.getenv('MMX_ANIMES').split(',') if os.getenv('MMX_ANIMES') else []
